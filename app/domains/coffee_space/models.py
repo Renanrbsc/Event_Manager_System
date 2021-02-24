09af78c40a1623bbed4644841a5dae0bcf975e7c
renan.ribas <renan.ribas@ambevtech.com.br>
2021-02-24 13:23:36 -0300

    creating models for student, screen, event_room and coffee_space

---
+++ app/domains/coffee_space/models.py
@@ -0,0 +1,28 @@
+from random import randint
+
+
+class CoffeeSpace:
+    _id = str()
+    _name = str()
+
+    def __init__(self, id: int = None, name: str = ""):
+        self.setId(id)
+        self.setName(name)
+
+    def setId(self, id: int) -> None:
+        if id:
+            self._id = '%04d' % id
+        else:
+            self._id = '%04d' % randint(0, 9999)
+
+    def getId(self) -> str:
+        return self._id
+
+    def setName(self, name: str) -> None:
+        self._name = name
+
+    def getName(self) -> str:
+        return self._name
+
+    def __str__(self) -> str:
+        return f"{self.getId()},{self.getName()}\n"
---
+++ app/domains/event_room/models.py
@@ -0,0 +1,39 @@
+from random import randint
+
+
+class EventRoom:
+    _id = str()
+    _name = str()
+    _capacity = str()
+
+    def __init__(self, id: int = None, name: str = "", capacity: int = None):
+        self.setId(id)
+        self.setName(name)
+        self.setCapacity(capacity)
+
+    def setId(self, id: int) -> None:
+        if id:
+            self._id = '%04d' % id
+        else:
+            self._id = '%04d' % randint(0, 9999)
+
+    def getId(self) -> str:
+        return self._id
+
+    def setName(self, name: str) -> None:
+        self._name = name
+
+    def getName(self) -> str:
+        return self._name
+
+    def setCapacity(self, capacity: int) -> None:
+        if capacity:
+            self._capacity = '%03d' % capacity
+        else:
+            self._capacity = '%03d' % 0
+
+    def getCapacity(self) -> str:
+        return self._capacity
+
+    def __str__(self) -> str:
+        return f"{self.getId()},{self.getName()},{self.getCapacity()}\n"
